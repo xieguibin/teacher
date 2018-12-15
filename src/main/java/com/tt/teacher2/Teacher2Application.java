22f02a9cff954501845f858b7b81d812c0a042ed
TSTank <m18903577547@163.com>
2018-12-15 16:47:26 +0800

    把项目创建上

---
+++ src/main/java/com/tt/teacher2/Teacher2Application.java
@@ -0,0 +1,14 @@
+package com.tt.teacher2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teacher2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teacher2Application.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/tt/teacher2/Teacher2ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tt.teacher2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Teacher2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
